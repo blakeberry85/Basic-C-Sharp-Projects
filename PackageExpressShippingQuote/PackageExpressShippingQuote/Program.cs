ed5df1e2e7ace3c3931e074c74ed66b01e9d34b6
Blake Berry <blakeberry85@yahoo.com>
2020-08-10 03:57:09 -0700

    Add Branching assignment

---
+++ PackageExpressShippingQuote/PackageExpressShippingQuote/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpressShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height:");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package length:");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+            int packageDimensionSum = packageWidth + packageHeight + packageLength;
+
+            if (packageDimensionSum > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            decimal packageDimensionProduct = packageWidth * packageHeight * packageLength;
+            decimal packageQuote = packageDimensionProduct * packageWeight / 100;
+            Console.WriteLine("Your estimated total for shipping this package is: $" + packageQuote.ToString("0.00"));
+            Console.WriteLine("Thank you.");
+            Console.ReadLine();
+        }
+    }
+}
