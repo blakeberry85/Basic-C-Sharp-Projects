8133eb22a84159bffe00bada8506b32c9e30947d
Blake Berry <blakeberry85@yahoo.com>
2020-09-09 00:55:45 -0700

    Add Exception Handling assignment

---
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int>() { 10, 50, 100, 200, 400, 800 };
+                Console.WriteLine("Enter a divisor");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numbers)
+                {
+                    int dividedNumber = number / divisor;
+                    Console.WriteLine(dividedNumber);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            Console.WriteLine("Continuing on...");
+            Console.ReadLine();
+        }
+    }
+}
