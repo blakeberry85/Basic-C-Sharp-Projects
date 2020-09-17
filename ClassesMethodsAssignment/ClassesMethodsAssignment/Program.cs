cd2a6483a4f5c34b5a069b8712272b745b97476a
Blake Berry <blakeberry85@yahoo.com>
2020-09-17 01:30:56 -0700

    Add Classes and Methods assignment

---
+++ ClassesMethodsAssignment/ClassesMethodsAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Enter a number to be operated on.");
+            math.userOperand = Convert.ToInt32(Console.ReadLine());
+            math = Multiply5(math);
+            Console.WriteLine(math.userOperand + " * 5 = " + math.result);
+            math = Add50(math);
+            Console.WriteLine("+ 50 = " + math.result);
+            math = Multiply2(math);
+            Console.WriteLine("* 2 = " + math.result);
+            Console.WriteLine("Your result is " + math.result);
+            Console.ReadLine();
+
+        }
+
+        public static Math Multiply5(Math math)
+        {
+            math.result = math.userOperand * 5;
+            return math;
+        }
+
+        public static Math Add50(Math math)
+        {
+            math.result = math.result + 50;
+            return math;
+        }
+
+        public static Math Multiply2(Math math)
+        {
+            math.result = math.result * 2;
+            return math;
+        }
+    }
+}
