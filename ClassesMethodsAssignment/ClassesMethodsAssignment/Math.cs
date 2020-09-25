c50caeb05dd1344408475b2721cf932a810195c7
Blake Berry <blakeberry85@yahoo.com>
2020-09-24 20:04:27 -0700

    Update Classes and Methods assignment
    Moved functions from the program to the math class.

--- ClassesMethodsAssignment/ClassesMethodsAssignment/Math.cs
+++ ClassesMethodsAssignment/ClassesMethodsAssignment/Math.cs
@@ -10,6 +10,24 @@ namespace ClassesMethodsAssignment
     {
         public int userOperand { get; set; }
         public int result { get; set; }
+
+        public static Math Multiply5(Math math)
+        {
+            math.result = math.userOperand * 5;
+            return math;
+        }
+
+        public static Math Add50(Math math)
+        {
+            math.result = math.result + 50;
+            return math;
+        }
+
+        public static Math Multiply2(Math math)
+        {
+            math.result = math.result * 2;
+            return math;
+        }
     }
 
 
--- ClassesMethodsAssignment/ClassesMethodsAssignment/Program.cs
+++ ClassesMethodsAssignment/ClassesMethodsAssignment/Program.cs
@@ -13,33 +13,15 @@ namespace ClassesMethodsAssignment
             Math math = new Math();
             Console.WriteLine("Enter a number to be operated on.");
             math.userOperand = Convert.ToInt32(Console.ReadLine());
-            math = Multiply5(math);
+            math = Math.Multiply5(math);
             Console.WriteLine(math.userOperand + " * 5 = " + math.result);
-            math = Add50(math);
+            math = Math.Add50(math);
             Console.WriteLine("+ 50 = " + math.result);
-            math = Multiply2(math);
+            math = Math.Multiply2(math);
             Console.WriteLine("* 2 = " + math.result);
             Console.WriteLine("Your result is " + math.result);
             Console.ReadLine();
 
-        }
-
-        public static Math Multiply5(Math math)
-        {
-            math.result = math.userOperand * 5;
-            return math;
-        }
-
-        public static Math Add50(Math math)
-        {
-            math.result = math.result + 50;
-            return math;
-        }
-
-        public static Math Multiply2(Math math)
-        {
-            math.result = math.result * 2;
-            return math;
         }
     }
 }
