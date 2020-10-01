ede44709eb564dd1db03040167b3ca22a075efde
Blake Berry <blakeberry85@yahoo.com>
2020-09-30 22:31:05 -0700

    Add MethodOverloadingAssignment

---
+++ MethodOverloadingAssignment/MethodOverloadingAssignment/Math.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingAssignment
+{
+    class Math
+    {
+        public static int Operations(int operand)
+        {
+            operand = operand - 4;
+            return operand;
+        }
+
+        public static int Operations(decimal operand)
+        {
+            operand = operand * 10;
+            return Convert.ToInt32(operand);
+        }
+
+        public static int Operations(string operand)
+        {
+            int convertOperand = 0;
+            try
+            {
+                convertOperand = Convert.ToInt32(operand);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That string cannot be converted to an integer!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            convertOperand = convertOperand / 3;
+            return convertOperand;
+        }
+    }
+}
---
+++ MethodOverloadingAssignment/MethodOverloadingAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math subtract4 = new Math();
+            int subtractResult = Math.Operations(10);
+            Console.WriteLine(subtractResult);
+            Console.ReadLine();
+
+            Math multiply10 = new Math();
+            int multiplyResult = Math.Operations(5.3m);
+            Console.WriteLine(multiplyResult);
+            Console.ReadLine();
+
+            Math divide3 = new Math();
+            int divideResult = Math.Operations("eightteen");
+            Console.WriteLine(divideResult);
+            Console.ReadLine();
+        }
+    }
+}
