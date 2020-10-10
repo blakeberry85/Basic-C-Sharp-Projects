4f47778cfc45f084b4456e8ba89be11f9b20ade3
Blake Berry <blakeberry85@yahoo.com>
2020-10-09 23:56:44 -0700

    Add Optional Parameter Assignment

---
+++ OptionalParameterAssignment/OptionalParameterAssignment/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterAssignment
+{
+    class Math
+    {
+        public int Multiply(int operand1, int operand2 = 2)
+        {
+            int multiplyResult = operand1 * operand2;
+            return multiplyResult;
+        }
+    }
+}
---
+++ OptionalParameterAssignment/OptionalParameterAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math multiply1 = new Math();
+            Console.WriteLine("Please enter two numbers one at a time. First number is required, second number will default to 2 if left blank.");
+            int operand1 = Convert.ToInt32(Console.ReadLine());
+            string operand2 = Console.ReadLine();
+            int multiplyResult = 0;
+            if (operand2 == String.Empty)
+            {
+                multiplyResult = multiply1.Multiply(operand1);
+            }
+            else
+            {
+                int convertOperand2 = Convert.ToInt32(operand2);
+                multiplyResult = multiply1.Multiply(operand1, convertOperand2);
+            }
+            Console.WriteLine("Your answer is " + multiplyResult);
+            Console.ReadLine();
+        }
+    }
+}
